981f038ebb8e81ee4ce8f1589d8472e4b8ac9653
B-Billings <b_billings@fanshaweonline.ca@users.noreply.github.com>
2021-10-31 16:14:46 -0400

    had to review class as i had many errors

--- main.py
+++ main.py
@@ -1,30 +1,86 @@
-from random import randint
-player = input("Chose rock, paper or scissors")
-player = input("player chose: " + player)
-print("player chose: " + player)
+from random import choice, randint
+from gameComponents import winLose
+
 choices = ["rock", "paper", "scissors"]
-computer = choices[randint(0,2)]
-print("computer chose" + computer)
-if (player == "rock"):
-    if (computer == "paper"):
-        print ("you lose")
-    elif (computer == "scissors"):
-        print("you win")
-    else:
+
+player = False
+
+# the lives decrease when round is lost
+playerLives = 5
+computerLives = 5
+
+while player is False:
+
+    player = input("Chose your weapon: rock, paper or scissors: ")
+    computer = choices[randint(0,2)]
+
+    print("player chose:  " + player)
+    print("computer chose:  " + computer)
+
+    if computer == player:
         print("tie try again")
+    elif (player == "rock"):
+        if (computer == "paper"):
+            print ("you lose")
+            playerLives = playerLives -1
+        else:
+            print("you win!")
+            computerLives = computerLives -1
 
-elif (player == "paper"):
-    if (computer == "scissors"):
-        print("you lose")
-    elif (computer == "rock"):
-        print ("you win!")
-    else:
-        print("tie!try again")
+    elif player == "paper":
+        if computer== "scissors":
+            print("you lose")
+            playerLives = playerLives -1
+        else:
+            print("you win")
+            computerLives = computerLives -1
 
-elif (player == "scissors"):
-    if (computer == "rock"):
-        print("you lose")
-    elif (computer == "paper"):
-        print ("you win!")
-    else:
-        print("tie!try again")
+    elif player == "paper":
+        if computer == "scissors":
+            print("you lose")
+            playerLives = playerLives -1
+        else:
+            print("you win!")
+            computerLives = computerLives -1
+
+    elif player == "scissors":
+        if computer == "rock":
+            print("you lose!")
+            playerLives = playerLives -1
+        else:
+            print("you win!")
+            computerLives = computerLives -1
+
+    print("player life count " + str(playerLives))
+    print("player life count " + str(computerLives))
+
+    if playerLives == 0:
+        winorlose("lost")
+
+        print("you lost! would you like to play again?")
+        choice = input(" Y / N ")
+
+        if choice == "n":
+            print*"better luck nexttime!"
+            exit()
+        
+        else:
+            playerLives = 5
+            computerLives = 5
+            player = False
+
+    elif computerLives == 0:
+        winorlose("won")
+        
+        print("you won! Would you like to play again?")
+        choice = input(" Y / N ")
+
+        if choice == "n":
+            print("better luck next time!")
+            exit()
+
+        else:
+            playerLives = 5
+            computerLives = 5
+            player = False
+
